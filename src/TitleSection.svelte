a255d2918e8bf053c90b9e59ad02f29bedaef334
Kate-lynS <katelynsookdeosingh@gmail.com>
2025-07-21 23:04:12 -0400

    Adding title section

---
+++ src/TitleSection.svelte
@@ -0,0 +1,8 @@
+<script>
+    import TitleCard from "../lib/TitleCard.svelte";
+    
+    const title = "Insuring The Future";
+    const subtitle = "Exploring the Correlation Between Insureance and Education";
+</script>
+
+<TitleCard {title} {subtitle} />
